0efb0a95adbd1da3248c3a3ccaf38850f559518f
Web-Mike007 <killer457694@gmail.com>
2025-02-11 21:56:19 +0600

    Delete tst directory

--- tst/js/main.js
+++
@@ -1,6 +0,0 @@
-//document.write("<h1>Hello new world!</h1>");
-//document.write("Hello  brooo...!");
-//alert("Who are you?")
-//document.getElementById('ch').addEventListener('click',function() {
-//    location.reload()
-//})ж
