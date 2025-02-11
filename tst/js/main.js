ff355963dd3ce3e533c6137cefb5e0a93bc8c6e9
Web-Mike007 <killer457694@gmail.com>
2025-02-11 21:29:21 +0600

    Add files via upload

---
+++ tst/js/main.js
@@ -0,0 +1,6 @@
+//document.write("<h1>Hello new world!</h1>");
+//document.write("Hello  brooo...!");
+//alert("Who are you?")
+//document.getElementById('ch').addEventListener('click',function() {
+//    location.reload()
+//})ж
